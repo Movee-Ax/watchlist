8704bf2116ac02ea8139a6fcd4932157073669aa
Movee <2753114732@qq.com>
2020-11-08 10:08:00 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
-from flask import Flask
+# from flask import Flask
+# app = Flask(__name__)
+#
+# @app.route('/home')
+# def hello():
+#     return '<h1>sadasdasdasdasddss 三四三sssAa aa三四三  s112a</h1>'
+# @app.route('/user/<name>')
+# def user_page(name):
+#   return 'User:%s' % name
+from flask import Flask, render_template
 app = Flask(__name__)
+name = 'Grey Li'
+movies = [
+{'title': 'My Neighbor Totoro', 'year': '1988'},
+{'title': 'Dead Poets Society', 'year': '1989'},
+{'title': 'A Perfect World', 'year': '1993'},
+{'title': 'Leon', 'year': '1994'},
+{'title': 'Mahjong', 'year': '1996'},
+{'title': 'Swallowtail Butterfly', 'year': '1996'},
+{'title': 'King of Comedy', 'year': '1999'},
+{'title': 'Devils on the Doorstep', 'year': '1999'},
+{'title': 'WALL-E', 'year': '2008'},
+{'title': 'The Pork of Music', 'year': '2012'},
+]
 
-@app.route('/home')
-def hello():
-    return '<h1>sadasdasdasdasddss 三四三sssAa aa三四三  s112a</h1>'
-@app.route('/user/<name>')
-def user_page(name):
-  return 'User:%s' % name
+@app.route('/')
+def index():
+    return render_template('index.html', name=name, movies=movies)
